9f50ac7380209f8bbb5cd10f3a7007aea65e158e
Caio Normando De Lucena <f95367c@comaugroup.com>
2020-06-12 01:03:43 -0300

    feature: added ngx webcam view

--- image-predicter/src/app/features/picture-capture/picture-capture.component.ts
+++ image-predicter/src/app/features/picture-capture/picture-capture.component.ts
@@ -1,20 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PictureCaptureService } from './services/picture-capture.service';
+import { Subject, Observable } from 'rxjs';
+import { WebcamImage, WebcamUtil } from 'ngx-webcam';
 
 @Component({
   selector: 'app-picture-capture',
   templateUrl: './picture-capture.component.html',
   styleUrls: ['./picture-capture.component.scss'],
 })
-export class PictureCaptureComponent {
+export class PictureCaptureComponent implements OnInit {
   selectedFile: File;
+  multipleCamsAvailable = false;
+  webcamImage: WebcamImage = null;
+  pictureCaptureTrigger = new Subject<any>();
+  imageUrl: string;
 
   constructor(private photoCaptureService: PictureCaptureService) {}
+
+  ngOnInit() {
+    WebcamUtil.getAvailableVideoInputs().then(
+      (mediaDevices: MediaDeviceInfo[]) => {
+        this.multipleCamsAvailable = mediaDevices && mediaDevices.length > 1;
+      }
+    );
+  }
   onFileSelected(event) {
     this.selectedFile = event.target.files[0];
   }
 
-  onUpload() {
-    this.photoCaptureService.emmitPhotoUploading(this.selectedFile);
+  onUpload(photo: WebcamImage) {
+    this.imageUrl = photo.imageAsDataUrl;
+    this.photoCaptureService.emmitPhotoUploading(this.imageUrl);
+  }
+
+  triggerPictureSnapshot() {
+    this.pictureCaptureTrigger.next();
+  }
+
+  get pictureTriggerObservable(): Observable<void> {
+    return this.pictureCaptureTrigger.asObservable();
   }
 }
